f801053eeb314dae2ea9b2f274705303c63056b0
Saurabh <saurabhgawande2016@gmail.com>
2023-01-05 22:07:16 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,19 @@ import React , {useState,useEffect} from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
- //code here
+  const [change, setChange] = useState(0)
+  // const [square, setSquare] = useState(0)
+ function click(event){
+    setChange (event.target.value);
+    // setSquare(change*change);
+ }
 
   return (
     <div id="main">
       <ChildComponent /> 
-       <input id='input' onChange={} />
+       <input id='input' onChange={click} />
     
-      <p id='output'>{}</p>
+      <p id='output'>{change*change}</p>
     </div>
   )
 }
